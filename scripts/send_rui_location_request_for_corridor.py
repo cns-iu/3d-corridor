f2b7589b8ab4e688068ed40c8c49d1448ecf691a
Lu <luchen_catherine@outlook.com>
2023-07-02 21:55:05 -0400

    client

--- scripts/send_rui_location_request_for_corridor.py
+++ scripts/send_rui_location_request_for_corridor.py
@@ -9,7 +9,7 @@ import collections
 import pandas as pd
 
 url_collision_detection = "http://localhost:8080/get-collisions"
-json_rui_locations = "./data/rui_locations.jsonld"
+json_rui_locations = "../data/rui_locations.jsonld"
 
 def send_requests(json_path):
 
@@ -19,10 +19,10 @@ def send_requests(json_path):
         graph = json_data['@graph']
         
         for donor in graph:
-            if 'sample' not in donor:
+            if 'samples' not in donor:
                 continue
                 
-            tissue_blocks = donor['sample']
+            tissue_blocks = donor['samples']
             for tissue_block in tissue_blocks:
                 if 'rui_location' not in tissue_block or not tissue_block['rui_location']:
                     continue
@@ -32,7 +32,7 @@ def send_requests(json_path):
     
     
     for rui_id, rui_location in dic_rui_locations.items():
-        post_response = requests.post(url=url_collision_detection, json=rui_location).json()
+        post_response = requests.post(url=url_collision_detection, json=rui_location)
     
 
 if __name__ == "__main__":
